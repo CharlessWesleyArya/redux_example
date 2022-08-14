939c752d5fca2b5acc016bcb2aa50514c14ddb16
Charless Wesley Arya <wesleyarya40@gmail.com>
2022-08-14 18:53:32 +0530

    created counter app with increment and decrement, and add by specific number also added

--- src/App.js
+++ src/App.js
@@ -6,13 +6,19 @@ function App() {
   const increment = () => {
     dispatch({ type: "INC" });
   };
-  const decrement = () => {};
+  const decrement = () => {
+    dispatch({ type: "DEC" });
+  };
+  const addBy = () => {
+    dispatch({ type: "ADD", payload: 10 });
+  };
   return (
     <div>
       <h1>Counter App</h1>
       <h2>{counter}</h2>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
+      <button onClick={addBy}>Add 10</button>
     </div>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,15 @@ import { createStore } from "redux";
 const reducersFn = (state = { counter: 0 }, action) => {
   //Synchronous Funtion
   //we should not mutate the original state
+  if (action.type === "INC") {
+    return { counter: state.counter + 1 };
+  }
+  if (action.type === "DEC") {
+    return { counter: state.counter - 1 };
+  }
+  if (action.type === "ADD") {
+    return { counter: state.counter + action.payload };
+  }
   return state;
 };
 
